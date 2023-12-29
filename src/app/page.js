1ae9edce6a6db81425795d40c47965d33e846f96
bella77-69 <mrspasceri@hotmail.com>
2023-12-28 20:33:19 -0800

    added Footer, styled

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Image from 'next/image'
 import logo from "../../public/images/logo8.png";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   <Image 
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+   {/* <Image 
     src={logo}
     alt="hero"
     width={500}
     height={500}
-    />
+    /> */}
     </main>
   )
 }
